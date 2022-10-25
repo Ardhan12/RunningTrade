9e8f59120959a6fd75b927782c9b99e1cbe6c4ea
Ardhan12 <arief.ramadhan43@gmail.com>
2022-10-26 06:21:23 +0700

    Initial Commit

---
+++ PhilipRunningTrade/PhilipRunningTradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhilipRunningTradeApp.swift
+//  PhilipRunningTrade
+//
+//  Created by Arief Ramadhan on 26/10/22.
+//
+
+import SwiftUI
+
+@main
+struct PhilipRunningTradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
